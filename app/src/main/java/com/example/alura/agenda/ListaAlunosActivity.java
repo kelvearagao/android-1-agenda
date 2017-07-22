7201431717bd5e4136f3ef37131fb6d0d394ca0f
kelvearagao <kelveargao@gmail.com>
2017-07-21 21:33:42 -0300

    Novo projeto de uma agenda.

---
+++ app/src/main/java/com/example/alura/agenda/ListaAlunosActivity.java
@@ -0,0 +1,22 @@
+package com.example.alura.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+
+        listaAlunos.setAdapter(adapter);
+    }
+}
