6d388d1f1d92d9d931ecccae2301a593560db095
kelvearagao <kelveargao@gmail.com>
2017-07-21 22:52:31 -0300

    criando formulario de alunos

---
+++ app/src/main/java/com/example/alura/agenda/FormularioActivity.java
@@ -0,0 +1,25 @@
+package com.example.alura.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Aluno salvo com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
